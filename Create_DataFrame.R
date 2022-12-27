8cd95b3a9b293de1fb587eb4ae6a92cb6499e9cd
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-12-27 12:16:30 -0500

    Rstudio - Display dataframe

--- Create_DataFrame.R
+++ Create_DataFrame.R
@@ -3,3 +3,12 @@
 names <- c("Jorge","Anabela","Daniel","Isabela")
 age <- c(62L,62L,26L,22L)
 people <- data.frame(names, age)
+
+# Display first rows of the data frame
+
+head(people)
+
+str(people)
+
+glimpse(people)
+
