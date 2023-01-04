b8be27622cd81dfcac3790ccea2ed6e3b5fbb6fe
stbnvlrd <estebanvelardegarcez@gmail.com>
2023-01-04 12:16:30 -0500

    Rstudio - Group by island and species

--- Clean_ColumnsNames.R
+++ Clean_ColumnsNames.R
@@ -41,3 +41,7 @@ penguins %>% group_by(species) %>% drop_na() %>% summarise(mean_bill_length_mm =
 
 penguins %>% group_by(species) %>% drop_na() %>% summarise(max_flipper_length_mm = max(flipper_length_mm))
 
+# Group by island and species
+
+penguins %>% group_by(species, island) %>% drop_na() %>% summarise(max_bill_length_mm = max(bill_length_mm), mean_bill_length_mm = mean(bill_length_mm))
+
