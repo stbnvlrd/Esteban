81215747a13a50114c0ce47a03cbb7305df8ef58
stbnvlrd <estebanvelardegarcez@gmail.com>
2023-01-04 12:16:30 -0500

    Rstudio - Filter by species

--- Clean_ColumnsNames.R
+++ Clean_ColumnsNames.R
@@ -45,3 +45,6 @@ penguins %>% group_by(species) %>% drop_na() %>% summarise(max_flipper_length_mm
 
 penguins %>% group_by(species, island) %>% drop_na() %>% summarise(max_bill_length_mm = max(bill_length_mm), mean_bill_length_mm = mean(bill_length_mm))
 
+############ Filter ############ 
+
+penguins %>% filter(species == "Adelie")
