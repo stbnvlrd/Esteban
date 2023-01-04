a31c65bca07dfd08352556676cfb467765597c50
stbnvlrd <estebanvelardegarcez@gmail.com>
2023-01-04 12:16:30 -0500

    Rstudio - Import CSV

---
+++ ggplot_2.R
@@ -0,0 +1,14 @@
+##########
+
+hotel_bookings <- read.csv("Rstudio/Import_Project/hotel_bookings.csv")
+
+##########
+
+head(hotel_bookings)
+colnames(hotel_bookings)
+
+##########
+
+install.packages('ggplot2')
+library(ggplot2)
+
