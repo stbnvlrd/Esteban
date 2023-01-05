e73364d238af0a3c31b7b79e3b012701660763a5
stbnvlrd <estebanvelardegarcez@gmail.com>
2023-01-05 12:16:30 -0500

    Rstudio - Plot stays in weekend vs children

--- ggplot_2.R
+++ ggplot_2.R
@@ -17,3 +17,5 @@ library(ggplot2)
 ggplot(data = hotel_bookings) +
   geom_point(mapping = aes(x = lead_time, y = children))
 
+ggplot(data = hotel_bookings) +
+  geom_point(mapping = aes(x = stays_in_weekend_nights, y = children))
